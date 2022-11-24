3997a4f468bd254819f6860571d90fdda7e50990
Mateo Pico Zuleta <picolive5@hotmail.com>
2022-11-23 21:42:56 -0500

    Agregar la api fake de productos

--- index.js
+++ index.js
@@ -7,4 +7,39 @@ let value = 0;
 boton.addEventListener('click',()=>{
     value++;
     counter.textContent = value; 
-})
+}) 
+
+
+const cargarProducts = async()=>{
+    
+const API = "https://fakestoreapi.com/products";
+    
+const HTMLresponse = document.getElementById("grid-products")
+
+    try{
+        const respuesta = await fetch(API)
+
+        if(respuesta.status===200){
+            const datos = await respuesta.json()
+
+            let productos = "";
+            datos.forEach(product =>{
+                productos += `<div class="card-products" id="card-products">
+                                <img class="image-product" src="${product.image}" alt="producto">
+                                <h1 class="title-card">${product.title}</h1>
+                                <p class="subtitle-card">${product.category}</p>
+                                <div class="price-button">
+                                    <div class="price-card">$${product.price}</div>
+                                    <button class="add-cart" id="addCart" >Add to cart</button>
+                                </div>
+                            </div>`
+            })
+            HTMLresponse.innerHTML = productos
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+cargarProducts()
